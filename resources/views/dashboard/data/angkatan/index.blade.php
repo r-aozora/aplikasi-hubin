948acfc850ce4cbc65897588ce2f44ea89223637
r-aozora <zytrahidayat11@gmail.com>
2023-10-14 15:54:40 +0700

    style: mengatur kolom untuk menu kelola data

--- resources/views/dashboard/data/angkatan/index.blade.php
+++ resources/views/dashboard/data/angkatan/index.blade.php
@@ -1,4 +1,4 @@
-<section id="data-angkatan" class="section col-6">
+<section id="data-angkatan" class="section col-lg-6 col-12">
     <div class="card">
         <div class="card-header">
             <div class="row">
--- resources/views/dashboard/data/index.blade.php
+++ resources/views/dashboard/data/index.blade.php
@@ -19,81 +19,80 @@
         </div>
     </div>
     <div class="page-content">
-        <section id="groups" class="mb-5">
-            <div class="row match-height">
-                <div class="col-12">
-                    <div class="card-group text-center mb-5">
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h4 class="card-title">Data Guru</h4>
-                                    <div class="card-text">
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importGuru">
-                                            <i class="bi bi-upload"></i>
-                                            Impor Data Guru
-                                        </button>
-                                        <a href="{{ route('guru.export') }}" class="btn btn-primary btn-sm">
-                                            <i class="bi bi-download"></i>
-                                            Download Data Guru
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h4 class="card-title">Data Siswa</h4>
-                                    <div class="card-text">
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importSiswa">
-                                            <i class="bi bi-upload"></i>
-                                            Impor Data Siswa
-                                        </button>
-                                        <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
-                                            <i class="bi bi-download"></i>
-                                            Download Data Siswa
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h5 class="card-title">Data Guru</h5>
                     </div>
-                    <div class="card-group text-center">
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h4 class="card-title">Data Perusahaan</h4>
-                                    <div class="card-text">
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importPerusahaan">
-                                            <i class="bi bi-upload"></i>
-                                            Impor Data Perusahaan
-                                        </button>
-                                        <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
-                                            <i class="bi bi-download"></i>
-                                            Download Data Perusahaan
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importGuru">
+                                <i class="bi bi-upload"></i>
+                                Impor Data Guru
+                            </button>
+                            <a href="{{ route('guru.export') }}" class="btn btn-primary btn-sm">
+                                <i class="bi bi-download"></i>
+                                Download Data Guru
+                            </a>
                         </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h4 class="card-title">Data Monitoring</h4>
-                                    <div class="card-text">
-                                        <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
-                                            <i class="bi bi-download"></i>
-                                            Download Data Monitoring
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
                     </div>
                 </div>
             </div>
-        </section>
+            <div class="col-md-6 col-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h4 class="card-title">Data Siswa</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importSiswa">
+                                <i class="bi bi-upload"></i>
+                                Impor Data Siswa
+                            </button>
+                            <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
+                                <i class="bi bi-download"></i>
+                                Download Data Siswa
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h4 class="card-title">Data Perusahaan</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#importPerusahaan">
+                                <i class="bi bi-upload"></i>
+                                Impor Data Perusahaan
+                            </button>
+                            <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
+                                <i class="bi bi-download"></i>
+                                Download Data Perusahaan
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h4 class="card-title">Data Monitoring</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <a href="{{ url('/') }}" class="btn btn-primary btn-sm">
+                                <i class="bi bi-download"></i>
+                                Download Data Monitoring
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         @include('dashboard.guru.import')
         @include('dashboard.perusahaan.import')
