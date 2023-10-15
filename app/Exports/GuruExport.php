0557319720cac5727555ec59a0cf500ef9da9743
r-aozora <zytrahidayat11@gmail.com>
2023-10-15 20:22:41 +0700

    chore: add WithMapping and WithHeadings in GuruImport

--- app/Exports/GuruExport.php
+++ app/Exports/GuruExport.php
@@ -3,20 +3,37 @@
 namespace App\Exports;
 
 use App\Models\Guru;
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class GuruExport implements FromView
+class GuruExport implements FromCollection, WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function view(): View
+    public function collection()
     {
-        $guru = Guru::orderBy('nama', 'asc')
-            ->get();
-            
-        return view('dashboard.guru.export')
-            ->with(['guru' => $guru]);
+        return Guru::orderBy('nama', 'asc')->get();
+    }
+
+    public function map($guru): array
+    {
+        return [
+            $guru->nama,
+            $guru->nip,
+            $guru->sebagai,
+            $guru->telepon,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'NAMA',
+            'NIP',
+            'SEBAGAI',
+            'TELEPON',
+        ];
     }
 }
