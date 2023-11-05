f204b3116dd1e1338fa878b493bad92359f04682
r-aozora <zytrahidayat11@gmail.com>
2023-11-05 14:32:52 +0700

    feat: CRUD data perusahaan dengan template stisla

--- resources/views/dashboard/perusahaan/edit.blade.php
+++ resources/views/dashboard/perusahaan/edit.blade.php
@@ -1,99 +1,120 @@
-<div class="modal fade" id="editPerusahaan{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="editPerusahaan{{ $item->id }}" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editPerusahaan{{ $item->id }}">
-                    Edit Data Perusahaan
-                </h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                    <i data-feather="x"></i>
-                </button>
+@extends('layouts.app')
+
+@section('link')
+    <link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
+@endsection
+
+@section('content')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <div class="section-header-back">
+                    <a href="{{ route('perusahaan.index') }}" class="btn btn-icon">
+                        <i class="fas fa-arrow-left"></i>
+                    </a>
+                </div>
+                <h1>{{ $title }}</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active">
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </div>
+                    <div class="breadcrumb-item">
+                        <a href="{{ route('perusahaan.index') }}">Data Perusahaan</a>
+                    </div>
+                    <div class="breadcrumb-item">Edit Data</div>
+                </div>
             </div>
-            <form class="form form-vertical" action="{{ route('perusahaan.update', $item->id) }}" method="post">
-                @csrf
-                @method('PUT')
-                <div class="modal-body">
-                    <div class="form-body">
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="nama">Nama Perusahaan</label>
-                                    <input type="text" id="nama" class="form-control" name="nama" value="{{ $item->nama }}">
-                                </div>
+            <div class="section-body">
+                <h2 class="section-title">{{ $title }}</h2>
+                <p class="section-lead">
+                    On this page you can create a new post and fill in all fields.
+                </p>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>{{ $title }}</h4>
                             </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="alamat">Alamat Perusahaan</label>
-                                    <input type="text" id="alamat" class="form-control" name="alamat" value="{{ $item->alamat }}">
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="penerima">Penerima</label>
-                                    <input type="text" id="penerima" class="form-control" name="penerima" value="{{ $item->penerima }}">
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="kecamatan">Kecamatan</label>
-                                    <input type="text" id="kecamatan" class="form-control" name="kecamatan" value="{{ $item->kecamatan }}">
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="kota">Kabupaten / Kota</label>
-                                    <input type="text" id="kota" class="form-control" name="kota" value="{{ $item->kota }}">
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="provinsi">Provinsi</label>
-                                    <input type="text" id="provinsi" class="form-control" name="provinsi" value="{{ $item->provinsi }}">
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="lokasi">Lokasi</label>
-                                    <select class="form-select" id="lokasi" name="lokasi">
-                                        <option value="Luar Kota"
-                                            @if ($item->lokasi === 'Luar Kota')
-                                                selected
-                                            @endif
-                                        >Luar Kota</option>
-                                        <option value="Dalam Kota"
-                                            @if ($item->lokasi === 'Dalam Kota')
-                                                selected
-                                            @endif
-                                        >Dalam Kota</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group">
-                                    <label for="telepon">Telepon Perusahaan</label>
-                                    <input type="text" id="telepon" class="form-control" name="telepon" value="{{ $item->telepon }}">
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="koordinat">Koordinat Perusahaan</label>
-                                    <input type="text" id="koordinat" class="form-control" name="koordinat" value="{{ $item->koordinat }}">
-                                </div>
+                            <div class="card-body">
+                                <form action="{{ route('perusahaan.update', $perusahaan->slug) }}" method="post">
+                                    @csrf
+                                    @method('put')
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama Perusahaan</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="nama" value="{{ $perusahaan->nama }}" required autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Penerima Surat</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="penerima" value="{{ $perusahaan->penerima }}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Alamat</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="alamat" value="{{ $perusahaan->alamat }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kecamatan</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="kecamatan" value="{{ $perusahaan->kecamatan }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kota/Kabupaten</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="kota" value="{{ $perusahaan->kota }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Provinsi</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="provinsi" value="{{ $perusahaan->provinsi }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Lokasi</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <select class="form-control selectric" name="lokasi" required>
+                                                <option value="Dalam" {{ $perusahaan->lokasi === 'Dalam' ? 'selected' : '' }}>Dalam Kota</option>
+                                                <option value="Luar" {{ $perusahaan->lokasi === 'Luar' ? 'selected' : '' }}>Luar Kota</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">No. Telepon</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="telepon" value="{{ $perusahaan->telepon }}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Koordinat</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="koordinat" value="{{ $perusahaan->koordinat }}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <button type="submit" class="btn btn-primary">Edit Data</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">
-                        <i class="bx bx-x d-block d-sm-none"></i>
-                        <span class="d-none d-sm-block">Batal</span>
-                    </button>
-                    <button type="submit" class="btn btn-primary btn-sm ms-1" data-bs-dismiss="modal">
-                        <i class="bx bx-check d-block d-sm-none"></i>
-                        <span class="d-none d-sm-block">Edit Data</span>
-                    </button>
-                </div>
-            </form>
-        </div>
+            </div>
+        </section>
     </div>
-</div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('assets/js/page/features-post-create.js') }}"></script>
+@endsection
--- resources/views/dashboard/perusahaan/index.blade.php
+++ resources/views/dashboard/perusahaan/index.blade.php
@@ -1,94 +1,113 @@
 @extends('layouts.app')
 
+@section('link')
+    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/datatables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css') }}">
+@endsection
+
 @section('content')
-    <div class="page-heading">
-        <div class="page-title">
-            <div class="row">
-                <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Data Perusahaan</h3>
-                </div>
-                <div class="col-12 col-md-6 order-md-2 order-first">
-                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Data Peusahaan</li>
-                        </ol>
-                    </nav>
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>{{ $title }}</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active">
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </div>
+                    <div class="breadcrumb-item">Data Perusahaan</div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="page-content">
-        <section class="section">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-6">
-                            <h5 class="card-title">
-                                Data Perusahaan
-                            </h5>
-                        </div>
-                        <div class="col-6 text-end">
-                            <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#tambahPerusahaan">
-                                <i class="bi bi-plus-circle"></i>
-                                Tambah Perusahaan
-                            </button>
+            <div class="section-body">
+                <h2 class="section-title">Kelola {{ $title }}</h2>
+                <p class="section-lead">
+                    We use 'DataTables' made by @SpryMedia. You can check the full documentation <a href="https://datatables.net/">here</a>.
+                </p>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>{{ $title }}</h4>
+                                <div class="card-header-action">
+                                    <a href="{{ route('perusahaan.create') }}" class="btn btn-sm btn-primary" data-toggle="tooltip" data-placement="top" title="Tambah Data Perusahaan">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+                                    <a href="{{ route('perusahaan.export') }}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Download Data Perusahaan">
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                    <button class="btn btn-sm btn-info" id="btn-import" data-toggle="tooltip" data-placement="top" title="Impor Data Perusahaan">
+                                        <i class="fas fa-upload"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped" id="table-1">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center">NO</th>
+                                                <th>PERUSAHAAN</th>
+                                                <th>ALAMAT</th>
+                                                <th>LOKASI</th>
+                                                <th>TELEPON</th>
+                                                <th>OPSI</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($perusahaan as $item)
+                                                <tr>
+                                                    <td class="text-center">{{ $loop->iteration }}</td>
+                                                    <td>{{ $item->nama }}</td>
+                                                    <td>{{ $item->alamat . ', Kec. ' . $item->kecamatan . ', ' . $item->kota . ', ' . $item->provinsi }}</td>
+                                                    <td>{{ $item->lokasi }} Kota</td>
+                                                    <td>{{ $item->telepon }}</td>
+                                                    <td>
+                                                        <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            OPSI
+                                                        </button>
+                                                        <div class="dropdown-menu dropleft">
+                                                            <a class="dropdown-item has-icon" href="{{ route('perusahaan.show', $item->slug) }}">
+                                                                <i class="fas fa-eye"></i>
+                                                                Detail
+                                                            </a>
+                                                            <a class="dropdown-item has-icon" href="{{ route('perusahaan.edit', $item->slug) }}">
+                                                                <i class="fas fa-edit"></i>
+                                                                Edit
+                                                            </a>
+                                                            <div class="dropdown-divider"></div>
+                                                            <a class="dropdown-item has-icon text-danger" href="{{ route('perusahaan.destroy', $item->slug) }}" data-confirm-delete="true">
+                                                                <i class="fas fa-trash" onclick="event.preventDefault(); this.closest('a').click();"></i>
+                                                                Hapus
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <table class="table table-striped" id="table1">
-                        <thead>
-                            <tr>
-                                <th>NO</th>
-                                <th>PERUSAHAAN</th>
-                                <th>ALAMAT</th>
-                                <th>LOKASI</th>
-                                <th>TELEPON</th>
-                                <th>ACTION</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php $i = 1 ?>
-                            @foreach ($perusahaan as $item)
-                                <tr>
-                                    <td>{{ $i++ }}</td>
-                                    <td>{{ strtoupper($item->nama) }}</td>
-                                    <td>{{ $item->alamat }}</td>
-                                    <td>{{ $item->lokasi }}</td>
-                                    <td>{{ $item->telepon }}</td>
-                                    <td>
-                                        <div class="btn-group mb-1">
-                                            <div class="dropdown">
-                                                <button class="btn btn-secondary btn-sm dropdown-toggle me-1" type="button" id="option" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="bi bi-error-circle me-50"></i>
-                                                    Opsi
-                                                </button>
-                                                <div class="dropdown-menu" aria-labelledby="option">
-                                                    <a href="{{ route('perusahaan.show', $item->id) }}" class="dropdown-item">
-                                                        <i class="bi bi-person-square"></i>
-                                                        Detail
-                                                    </a>
-                                                    <button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#editPerusahaan{{ $item->id }}">
-                                                        <i class="bi bi-pencil-square"></i>
-                                                        Edit
-                                                    </button>
-                                                    <a href="{{ route('perusahaan.destroy', $item->id) }}" class="dropdown-item" data-confirm-delete="true">
-                                                        <i class="bi bi-trash"></i>
-                                                        Hapus
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        @include('dashboard.perusahaan.edit')
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </section>
     </div>
-    @include('dashboard.perusahaan.create')
+
+    @include('dashboard.perusahaan.import')
+@endsection
+
+@section('script')
+    <script src="{{ asset('assets/modules/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/jquery-ui/jquery-ui.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('assets/js/page/modules-datatables.js') }}"></script>
+    <script>
+        document.getElementById('btn-import').addEventListener('click', function () {
+            $('#importPerusahaan').modal('show');
+        });
+    </script>
 @endsection
